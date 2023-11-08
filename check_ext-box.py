e35b5f81decd1a75ffd8754be558e0dce541f922
Viktor Bukarev <bukarev1985@gmail.com>
2023-11-08 14:41:07 +0600

    Я написал часть кода для кейса

--- check_ext-box.py
+++ check_ext-box.py
@@ -1,1 +1,18 @@
-a
+import time
+
+from selene import browser
+from selene import  have
+
+a = 'Bukarev Viktor Vladimirovich'
+e = 'bukarev1985@gmail.com'
+address = ' 1, testovaya str, Almaty, Kazakhstan'
+address2 = '1, testovaya str, Almaty, Kazakhstan'
+
+def test_check_ext_box():
+    browser.open('https://demoqa.com/text-box')
+    browser.element('[id="userName"]').type(a)
+    browser.element('[id="userEmail"]').type(e)
+    browser.element('//*[@id="currentAddress-wrapper"]/div[2]').type(address)
+    browser.element('//*[@id="permanentAddress-wrapper"]/div[2]').type(address2)
+    browser.element('[id="submit"]').click()
+    time.sleep(45)
