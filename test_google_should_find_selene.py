f9ec4ee154af911baf0c45ec15400eeea923ae63
Viktor Bukarev <bukarev1985@gmail.com>
2023-11-08 14:03:33 +0600

    Я добавлю новый кейс проверка ext-box

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -5,4 +5,3 @@ def test_example():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
-
