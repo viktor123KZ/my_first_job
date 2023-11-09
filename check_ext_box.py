6fbdf42c4ec924e2346a6be681aed9dac6567f95
Viktor Bukarev <bukarev1985@gmail.com>
2023-11-09 21:05:23 +0600

    я создал файл что бы собрать туда много тестов

--- check_ext_box.py
+++ check_ext_box.py
@@ -12,7 +12,8 @@ address2 = '2, testovaya str, Almaty, Kazakhstan'
 @pytest.fixture
 def size_windows():
     options = webdriver.ChromeOptions()
-    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--window-size=960,540")
+    options.add_argument('--headless')
     browser.config.driver_options = options
     return browser
 
@@ -24,7 +25,7 @@ def test1(size_windows):
     browser.element('[id="userEmail"]').type(e)
     browser.element('//*[@id="currentAddress-wrapper"]/div[2] //*[@id="currentAddress"]').type(address)
     browser.element('//*[@id="permanentAddress-wrapper"]/div[2] //*[@id="permanentAddress"]').type(address2)
-    browser.element('[id="submit"]').click()
+    browser.element('//*[@id="submit"]').click()
     browser.element('//*[@id="name"]').should(have.exact_text('Name:Bukarev Viktor Vladimirovich'))
     browser.element('//*[@id="email"]').should(have.exact_text('Email:bukarev1985@gmail.com'))
     browser.element('//*[@id="output"]/div //*[@id="currentAddress"]').should(have.exact_text('Current Address :1, testovaya str, Almaty, Kazakhstan'))
